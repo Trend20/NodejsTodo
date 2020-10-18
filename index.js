57446352876a91b4fa57cdf41210e768373b1428
devenock <enockomondi305@gmail.com>
2020-10-18 00:03:48 -0400

    adding the page routes

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
 const express = require('express');
 const app = express();
-
+const bodyParser = require('body-parser');
+const { urlencoded } = require('body-parser');
 // define the port
 const port = 5000;
 
 // setting an ingine
 app.set('view engine', 'ejs');
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 // routes
 // GET
 
-app.get('/', function(req, res) {
-    res.send('Welcome to your first application using Nodejs');
+app.get("/", function(req, res) {
+    res.render("index", { task: task, complete: complete });
+});
+
+// task array
+var task = ["buy socks", "practise with nodejs"];
+
+app.post('/addtask', function(req, res) {
+    var newTask = req.body.newtask;
+    //add the new task from the post route into the array
+
+    task.push(newTask);
+
+    //after adding to the array go back to the root route
+    res.redirect("/");
 });
 
 // listen to the port
